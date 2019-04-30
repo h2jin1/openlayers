632fc4bad5b5657e568adbf2043710cf15b2e76b
HJCHOI <hjchoi@gaia3d.com>
2019-04-30 14:05:52 +0900

    point 실습 준비

--- src/main/webapp/js/point.js
+++ src/main/webapp/js/point.js
@@ -1,3 +1,29 @@
 /**
+ * openlayers 점으로 만든 예제
  * 
+	 * 1. layer 정의
+     * 2. projection 정의
+     * 3. view 정의
+     * 4. map 정의
  */
+
+
+var osm_layer = new ol.layer.Tile({
+	source: new ol.source.OSM()
+});
+
+var korea_5186 = ol.proj.transform([126.977234, 37.490115], 'EPSG:4326', 'EPSG:3857')
+var view = new ol.View({
+  center: korea_5186,
+  zoom: 10
+});
+
+var map = new ol.Map({
+  target: 'map'
+});
+
+map.addLayer(osm_layer);
+map.setView(view);
+
+
+
